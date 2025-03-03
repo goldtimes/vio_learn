ae6c57a688afb5c731edf31d5d654e2e01aed47d
lihang <lihang@kilox.cn>
2025-03-03 12:56:08 +0800

    "优化问题顶点的定义"

---
+++ src/vio_slam/src/backend/vertex.cc
@@ -0,0 +1,23 @@
+#include "vertex.hh"
+
+namespace vslam::backend {
+// 全局变量
+unsigned long global_vertex_id_ = 0;
+Vertex::Vertex(int num_dimension, int local_dimension) {
+    values_.resize(num_dimension, 1);
+    local_dimension = local_dimension > 0 ? local_dimension : num_dimension;
+    id_ = global_vertex_id_++;
+}
+
+int Vertex::Dimension() const {
+    return values_.rows();
+}
+
+int Vertex::LocalDimension() const {
+    return local_dimension_;
+}
+
+void Vertex::Plus(const VecX &delta) {
+    values_ += delta;
+}
+}  // namespace vslam::backend
