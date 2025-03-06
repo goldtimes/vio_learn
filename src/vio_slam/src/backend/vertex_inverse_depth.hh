d8710db885a2b01c1bbf1f264e84b348711864b6
lihang <lihang@kilox.cn>
2025-03-06 19:25:32 +0800

    "特征点的顶点形式xyz或者逆深度"

---
+++ src/vio_slam/src/backend/vertex_inverse_depth.hh
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "vertex.hh"
+
+// 以xyz形式存储的特征点形式
+namespace vslam::backend {
+class VertexInverseDepth : public Vertex {
+   public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+
+    VertexInverseDepth() : Vertex(1) {
+    }
+    // 更新方式默认的+法
+
+    virtual std::string TypeInfo() const override {
+        return "VertexInverseDepth";
+    }
+};
+}  // namespace vslam::backend
