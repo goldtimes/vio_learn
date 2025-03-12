9c98facf83810af2bc3ca7a5a4c321c1c01eeec9
lihang <lihang@kilox.cn>
2025-03-12 10:19:51 +0800

    "四元素的左右乘法"

--- src/vio_slam/src/vins_code/utility.hh
+++ src/vio_slam/src/vins_code/utility.hh
@@ -99,7 +99,16 @@ class Utility {
         return Rz * Ry * Rx;
     }
 
-    static Eigen::Matrix3d g2R(const Eigen::Vector3d &g);
+    static Eigen::Matrix3d g2R(const Eigen::Vector3d &g) {
+        Eigen::Matrix3d R0;
+        Eigen::Vector3d ng1 = g.normalized();
+        Eigen::Vector3d ng2{0, 0, 1.0};
+        R0 = Eigen::Quaterniond::FromTwoVectors(ng1, ng2).toRotationMatrix();
+        double yaw = Utility::R2ypr(R0).x();
+        R0 = Utility::ypr2R(Eigen::Vector3d{-yaw, 0, 0}) * R0;
+        // R0 = Utility::ypr2R(Eigen::Vector3d{-90, 0, 0}) * R0;
+        return R0;
+    }
 
     template <size_t N>
     struct uint_ {};
